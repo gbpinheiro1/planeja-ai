170b3fc834503a9e33c8ae0d58ed6d7e0fb9e72c
gbpinheiro1 <gabrielpinheiro551@gmail.com>
2026-07-24 21:15:10 -0300

    feat: adds react-router and the main routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router"
+import { router } from "./router"
+
 function App() {
-  return <h1>Planeja Ai</h1>
+  return <RouterProvider router={router} />
 }
 
 export default App
