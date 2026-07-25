117768668aaec331bc2fd0f938c61f1c97bb4db8
gbpinheiro1 <gabrielpinheiro551@gmail.com>
2026-07-24 21:58:21 -0300

    feat: creates button component and its variants

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router"
+import { Button } from "./components/shared/Button"
+import { PiggyBankIcon } from "lucide-react"
 
 export const router = createBrowserRouter([
   {
     children: [
       {
         path: "/",
-        element: <h1>Formulário de Simulação</h1>,
+        element: (
+          <>
+            <h1>Formulário de Simulação</h1>,{" "}
+          </>
+        ),
       },
       {
         path: "/resultado",
