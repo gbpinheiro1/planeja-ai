1ef62303888a934d0c21396fc8e2a6211dc869c0
gbpinheiro1 <gabrielpinheiro551@gmail.com>
2026-07-28 21:04:33 -0300

    feat: adds the question card and the form input

--- src/components/features/simulation/Form.tsx
+++ src/components/features/simulation/Form.tsx
@@ -1,9 +1,17 @@
+import { PiggyBank } from "lucide-react"
 import { StepProgress } from "../simulation/Progress"
+import { FormStep } from "./FormStep"
 
 export const SimulationForm = () => {
   return (
     <>
       <StepProgress currentStep={1} totalSteps={10}></StepProgress>
+      <FormStep
+        icon={PiggyBank}
+        title="Salário"
+        question="Quanto você ganha por mês?"
+        inputProps={{ type: "text", placeholder: "ex: 5.000,00", prefix: "R$" }}
+      ></FormStep>
     </>
   )
 }
